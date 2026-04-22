ebec9b436440c5df3cb93fb46eb51a6d555a3c62
Sahaj Dubey <dubeysahaj81@gmail.com>
2026-04-23 03:46:10 +0530

    Add Partition Labels 763 (1.Two Pointer)

---
+++ 1. Two Pointer/PartitionLabels_763.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PartitionLabels_763 {
+
+    public static List<Integer> partitionLabels(String s) {
+        List<Integer> result = new ArrayList<>();
+
+        int[] lastIndex = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex[s.charAt(i) - 'a'] = i;
+        }
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, lastIndex[s.charAt(i) - 'a']);
+
+            if (i == end) {
+                result.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "ababcbacadefegdehijhklij";
+        System.out.println(partitionLabels(s));
+    }
+}
